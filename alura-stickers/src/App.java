d4591eab3f9f5bf7ed01a1154f7f6cc7fef54407
J-Pedr0 <jpnolasco70@gmail.com>
2023-03-29 09:57:06 -0300

    segunda aula

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -6,12 +10,11 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar os top 250 filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -24,21 +27,52 @@ public class App {
 
         // exibir e manupular os dados
 
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();
+
         System.out.println("Filmes: ");
 
         for (int i = 0; i < listaDeFilmes.size(); i++) {
-            Map<String,String> filme = listaDeFilmes.get(i); 
-            System.out.println("\u001b[1mRank:\u001b[m "+ "  \u001b[31;1m" + "\u001b[4m" + filme.get("rank") + "\u001b[m");
-            System.out.println("\u001b[1mTitle:\u001b[m "+ " \u001b[32;1m" + "\u001b[4m" + filme.get("title") + "\u001b[m");
-            System.out.println("\u001b[1mYear:\u001b[m " + " \u001b[32;1m" + "\u001b[4m" + filme.get("year") + "\u001b[m");
-            // System.out.println(""+filme.get("image"));
-            System.out.print("\u001b[1mIMDB Rating:\u001b[m" + "\u001b[37;1m \u001b[33;1m" + "\u001b[4m" + filme.get("imDbRating") + "\u001b[m");
+            Map<String, String> filme = listaDeFilmes.get(i);
+
+            String urlImage = filme.get("image");
+            String rank = filme.get("rank");
+
+            InputStream inputStream = new URL(urlImage).openStream();
+            String nomeArquivo = "figurinhas/" + rank + ".png";
 
             double classificação = Double.parseDouble(filme.get("imDbRating"));
             int numeroEstrela = (int) classificação;
 
+            String textoFigurinha;
+
+            InputStream imagemJose;
+
+            if (numeroEstrela >= 8) {
+                textoFigurinha = "TOPZERA";
+                imagemJose = new FileInputStream(new File("C:/Serrateco/ImersaoJava/Aula1/alura-stickers/sobreposicao/topzera.jpg"));
+            } else {
+                textoFigurinha = "HMMMMMM...";
+                imagemJose = new FileInputStream(new File("C:/Serrateco/ImersaoJava/Aula1/alura-stickers/sobreposicao/hmm.jpg"));
+            }
+
+            var geradora = new GeradoraDeFigurinhas();
+            geradora.cria(inputStream, nomeArquivo, textoFigurinha, imagemJose);
+
+            System.out.println(
+                    "\u001b[1mRank:\u001b[m " + "  \u001b[31;1m" + "\u001b[4m" + filme.get("rank") + "\u001b[m");
+            System.out.println(
+                    "\u001b[1mTitle:\u001b[m " + " \u001b[32;1m" + "\u001b[4m" + filme.get("title") + "\u001b[m");
+            System.out.println(
+                    "\u001b[1mYear:\u001b[m " + " \u001b[32;1m" + "\u001b[4m" + filme.get("year") + "\u001b[m");
+            // System.out.println(""+filme.get("image"));
+            System.out.print("\u001b[1mIMDB Rating:\u001b[m" + "\u001b[37;1m \u001b[33;1m" + "\u001b[4m"
+                    + filme.get("imDbRating") + "\u001b[m");
+
+            
+
             for (int n = 1; n <= numeroEstrela; n++) {
-                System.out.print(" ⭐ ");    
+                System.out.print(" ⭐ ");
             }
             System.out.println("\n");
 
---
+++ alura-stickers/src/GeradoraDeFigurinhas.java
@@ -0,0 +1,83 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+
+    public void cria(InputStream inputStream, String nomeArquivo, String texto, InputStream inputStreamSobreposicao) throws Exception {
+
+        // leitura da imagem
+        // InputStream inputStream = new FileInputStream(
+        // new File("C:/Serrateco/ImersaoJava/Aula1/alura-stickers/entrada/filme.jpg"));
+
+        // InputStream inputStream = new
+        // URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
+
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // cria nova imagem em memória com tranparência e com tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+
+        int novaAltura = altura + 200;
+
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // copiar a imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        BufferedImage imagemSobreposicao = ImageIO.read(inputStreamSobreposicao);
+        int posicaoImagemSobreposicaoY = novaAltura - imagemSobreposicao.getHeight();
+        graphics.drawImage(imagemSobreposicao, 0, posicaoImagemSobreposicaoY, null);
+
+        // configurar a fonte
+        var fonte = new Font("impact", Font.BOLD, 200);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imgem
+        // String texto = "TOPZERA";
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D retangulo = fontMetrics.getStringBounds(texto, graphics);
+        int larguraTexto = (int) retangulo.getWidth();
+        int posicaoTextoX = (largura - larguraTexto) / 2;
+        int posicaoTextoY = (novaAltura - 50);
+
+        graphics.drawString(texto, posicaoTextoX, posicaoTextoY);
+
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        var textLayout = new TextLayout(texto, fonte, fontRenderContext);
+
+        Shape outline = textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(posicaoTextoX, posicaoTextoY);
+        graphics.setTransform(transform);
+
+        var outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+        graphics.setClip(outline);
+
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File("C:/Serrateco/ImersaoJava/Aula1/alura-stickers/" + nomeArquivo));
+
+        // /figurinha.png"
+
+    }
+
+}
